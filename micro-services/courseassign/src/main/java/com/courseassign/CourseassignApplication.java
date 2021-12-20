14416b167590f65423773e5626d2453f1ee3367f
Kakarot <mayankit3@gmail.com>
2021-12-20 12:33:08 +0530

    first commit

---
+++ micro-services/courseassign/src/main/java/com/courseassign/CourseassignApplication.java
@@ -0,0 +1,13 @@
+package com.courseassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseassignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseassignApplication.class, args);
+	}
+
+}
---
+++ micro-services/courseassign/src/test/java/com/courseassign/CourseassignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseassignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
